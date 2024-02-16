3fde8d0f9a67cb1d7d7d949053092673c5aba4d4
Yash Nandkuliyar <nandkuliyar1@gmail.com>
2024-02-16 21:27:41 +0530

    updates_initializati

---
+++ Arrays/array_declaration_loops_c.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int arr_int[5];
+    char arr_char[5];
+    float arr[5];
+    for (int i = 0; i < 5; i++)
+    {
+        arr[i]=(float)i*2.1;
+    }
+    
+    return 0;
+}
+
