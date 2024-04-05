36d53fb4d5866ee32278673148d6ba390e48cf26
Yash Nandkuliyar <nandkuliyar1@gmail.com>
2024-04-06 03:25:46 +0530

    updates

--- trees/tree_nodes.cpp
+++ trees/tree_nodes.cpp
@@ -9,11 +9,26 @@ struct Node
 void inorder(Node* root)
 {
     if(root==NULL)return ;
-    cout<<root->val<<" ";
     inorder(root->left);
     cout<<root->val<<" ";
     inorder(root->right);
     cout<<root->val<<" ";
+
+}
+
+int height(Node* root)
+{
+    if(root==NULL) return 0;
+    int heightleft=height(root->left);
+    int heightright=height(root->right);
+    return max(heightleft,heightright)+1;
+}
+void invert(Node* root)
+{
+    if(root==NULL) ;
+    invert(root->left);
+    invert(root->right);
+    swap(root->left,root->right);
 
 }
 int main()
@@ -32,5 +47,9 @@ int main()
     node4 ->val =13;
     node1->left=node3;
     node1->right=node3;
+    inorder(root);
+    cout<<endl;
+    cout<<height(root)-1;
+    invert(root);
     inorder(root);
 }
