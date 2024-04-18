4a86a81236bc2d7292857b0a99a9124637c57c51
Yash Nandkuliyar <nandkuliyar1@gmail.com>
2024-04-18 13:27:48 +0530

    added a lops folder for testing

---
+++ loops/user_input_while_loop.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+
+    int num;
+    scanf("%d", & num);
+    int a = 0;
+    while (a < num) {
+        printf("%d\n", a);
+        a = a + 1;
+    }
+}
---
+++ loops/while_loop.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+	int a = 0;
+	while (a<7){
+	    
+	printf("%d\n",a);
+	a++;
+	}
+}
