a977529a053f830af61ab37917c41f230bb9c6f1
Yash Nandkuliyar <nandkuliyar1@gmail.com>
2024-04-18 21:21:30 +0530

    j

---
+++ conditionals/wierd_not.py
@@ -0,0 +1,14 @@
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+
+    if n % 2 == 1:
+        print("Weird")
+    elif 2 <= n <= 5 and n % 2 == 0:
+        print("Not Weird")
+    elif 6 <= n <= 20 and n % 2 == 0:
+        print("Weird")
+    else:
+        print("Not Weird")
