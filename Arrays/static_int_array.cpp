6f9100df6b8047f6bbf9bdf128d3a9afe7ae660b
Yash Nandkuliyar <121722366+Yash-Nandkuliyar@users.noreply.github.com>
2024-05-14 00:42:44 +0530

    updates

---
+++ Arrays/static_int_array.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+   int intArray[4] = {5,6,7,8};
+   cout << "Array Elements: ";
+   for (int j = 0; j < 4; j++) {
+       cout << intArray[j];
+       cout << " ";
+   }
+  
+   return 0;
+}
